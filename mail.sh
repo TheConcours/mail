d3566dae4edc51c954e6a94fd8a36ab9f7be0504
TheConcours <kazuki.hamada@sie.co.jp>
2023-08-24 21:46:28 +0900

    Added shell scripts

---
+++ mail.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Constants
+Mailbox=~/Mailbox
+Messages=~/messages
+DefaultThreshold=18 # in MB
+DefaultMax=20 # in MB
+Threshold=$DefaultThreshold
+Max=$DefaultMax
+
+# help message
+usage() { echo "Usage: [-t ThresholdMB] [-m MaxMB]" 1>&2; exit 1; }
+# Getting parameters if exit
+while getopts t:m: option
+do 
+    case "${option}"
+        in
+        t)Threshold=${OPTARG};;
+        m)Max=${OPTARG};;
+	*)usage;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+echo $Max
+echo $Threshold
+# Script body
+cd $Mailbox
+echo $PWD
+du -d 2 --block-size=1M . | grep "/maildir" | sed -e 's=\./==g' | sed -e 's=/.*==g' | \
+awk -v th="$Threshold" -v max="$Max" -v mes="$Messages" '{
+  if ($1 >= th)
+    system("echo " $1 "/" max "MB > " mes "/" $2 ".txt")
+}'
+echo "There are `ls $Messages | wc -l` user accounts to be alerted"
---
+++ mailTest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Constants
+Mailbox=~/Mailbox
+
+# Initialize
+rm -rf $Mailbox
+
+# Create User maildir by generating random names
+for j in {1..50}
+do
+  i=`echo $RANDOM | base64 | tr -d '=+/.'`
+  mkdir -p $Mailbox/$i/maildir
+done
+echo "Created `ls $Mailbox | wc -l` dummy users"
+
+# Create dummy data
+cd $Mailbox
+for d in *; do
+  fallocate -l $(($RANDOM*1000)) $d/maildir/dummy.data
+done
+
